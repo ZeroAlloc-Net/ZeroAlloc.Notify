6428def25b58d3d80e413f0c59cfbb1e29f0e7a8
Marcel Roozekrans <marcel@roozekrans.nl>
2026-03-27 18:24:54 +0100

    feat: add OldValue and NewValue to AsyncPropertyChangingEventArgs

--- src/ZeroAlloc.Notify/AsyncPropertyChangingEventArgs.cs
+++ src/ZeroAlloc.Notify/AsyncPropertyChangingEventArgs.cs
@@ -3,5 +3,13 @@ namespace ZeroAlloc.Notify;
 public sealed class AsyncPropertyChangingEventArgs
 {
     public string PropertyName { get; }
-    public AsyncPropertyChangingEventArgs(string propertyName) => PropertyName = propertyName;
+    public object? OldValue { get; }
+    public object? NewValue { get; }
+
+    public AsyncPropertyChangingEventArgs(string propertyName, object? oldValue, object? newValue)
+    {
+        PropertyName = propertyName;
+        OldValue = oldValue;
+        NewValue = newValue;
+    }
 }
